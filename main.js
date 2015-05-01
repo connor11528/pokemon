0dbe7968d791f47b194b03db1fd8a2e2d2d868f3
Connor Leech <connorleech@gmail.com>
2015-05-01 13:49:54 -0700

    search and display pokemon

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	var POKEMON_BASE = "http://pokeapi.co/api/v1";
+	var POKEMON_BASE = "http://pokeapi.co/";
 
 	var generatePokemonIds = function(num){
 		var randomPokemonIds = [];
@@ -8,12 +8,13 @@ $(document).ready(function(){
 			randomPokemonIds.push(Math.floor(Math.random() * (720 - 2)) + 2);
 		}
 		return randomPokemonIds;
-	}
-	var getPokemonImages = function(num, callback){
-		var _randomPokemonIds = generatePokemonIds(num);
+	};
 
-		_randomPokemonIds.forEach(function(pokemonId, i){
-			var reqUrl = POKEMON_BASE + "/sprite/" + pokemonId;
+	var getPokemonImages = function(num, callback){
+		var randomPokemonIds = generatePokemonIds(num);
+
+		randomPokemonIds.forEach(function(pokemonId, i){
+			var reqUrl = POKEMON_BASE + "api/v1/sprite/" + pokemonId;
 
 			$.ajax({
 				url: reqUrl,
@@ -30,11 +31,65 @@ $(document).ready(function(){
 
 		getPokemonImages(5, function(imageData){
 			var $img = $('<img id="' + imageData.id +'">');
-			var imgUrl = 'http://pokeapi.co/' + imageData.image;
 
-			$img.attr('src', imgUrl).appendTo('#image-container');
+			$img.attr('src', POKEMON_BASE + imageData.image).appendTo('#image-container');
 		});
 		
 	});
-	
+
+	var displayPokemon = function(profileData){
+		$('dl').empty();
+		var profileInfo = '';
+		for(var key in profileData){
+			profileInfo += '<dt>' + key + '</dt>' + '<dd>' + JSON.stringify(profileData[key]) + '</dd>'
+		}
+		$('dl').append(profileInfo);
+	};
+
+	// get all pokemon
+	var $search = $('.search');
+	$.ajax({
+		url: POKEMON_BASE + "api/v1/pokedex/1/",
+		success: function(allPokemon){
+			console.log(allPokemon.pokemon);
+			var pokeList = '';
+			allPokemon.pokemon.forEach(function(pokemon, i){
+				pokeList += '<li class="list-group-item" id="' + pokemon.resource_uri + '">' + pokemon.name + '</li>'
+			});
+			$('.side-list').append(pokeList);
+
+			// search functionality
+			$search.removeClass('disabled');
+			$search.on('click', function(){
+				var searchTerm = $('.search-field').val().toLowerCase();
+				allPokemon.pokemon.forEach(function(pokemon, i){
+					if (pokemon.name === searchTerm){
+						$.ajax({
+							url: POKEMON_BASE + pokemon.resource_uri,
+							success: displayPokemon,
+							error: function(err){
+								alert('Whoops! ' + err)
+							}
+						});
+					}
+				});
+			});
+		}
+	});
+
+	$('.side-list').on('click', '.list-group-item', function(){
+		var _this = $(this);
+		_this.siblings().css('background-color', 'white');
+		_this.css('background-color', 'steelblue');
+		var resource_uri = _this.attr('id');
+		$.ajax({
+			url: POKEMON_BASE + resource_uri,
+			success: displayPokemon,
+			error: function(err){
+				alert('Whoops! ' + err)
+			}
+		});
+
+	})
 });
+
